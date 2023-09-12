4366bab8e1477cea39199a180a4cc5d71f15ee1c
Francisco <frannascimentomartins@gmail.com>
2023-09-12 17:12:58 -0300

    Aula 10

--- app/Models/Cliente.ts
+++ app/Models/Cliente.ts
@@ -3,7 +3,7 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Cliente extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
   public user_id: number;
