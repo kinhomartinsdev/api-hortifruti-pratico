5610e4d130a8fc9f39b00e7f0f731fc436a14ba0
Francisco <frannascimentomartins@gmail.com>
2023-09-15 11:46:51 -0300

    Aula 13

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,5 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Admin from 'App/Models/Admin';
+import Cliente from 'App/Models/Cliente';
+import Estabelecimento from 'App/Models/Estabelecimento';
 import User from 'App/Models/User';
+
 
 export default class AuthController {
     public async login({auth, request, response}: HttpContextContract){
@@ -58,8 +62,47 @@ export default class AuthController {
 
     }   
     public async me({auth, response}: HttpContextContract){
-        return response.ok(auth.user);}
 
+    const userAuth = await auth.use("api").authenticate();
+    
+        let data;
+        switch(userAuth.tipo){
+            case  "clientes": 
+            const cliente = await Cliente.findByOrFail('user.id' , userAuth.id);
+             data = {
+             id_cliente: cliente.id, 
+             nome: cliente.nome,
+             telefone: cliente.telefone,
+             email: userAuth,
+             }
+             break;
+
+             case  "estabelecimentos": 
+             const estabelecimento = await Estabelecimento.findByOrFail('user.id' , userAuth.id);
+             data = {
+                id_estabelecimento: estabelecimento.id, 
+                nome: estabelecimento.nome,
+                logo: estabelecimento.logo,
+                online: estabelecimento.online,
+                bloqueado: estabelecimento.bloqueado,
+                email: userAuth.email};
+                break;
+
+                case  "admins": 
+             const admin = await Admin.findByOrFail('user.id' , userAuth.id);
+             data = {
+                id_admin: admin.id,
+                nome: admin.nome,
+                emai: userAuth.email
+                
+            }  
+            break;
+            default :
+            return response.badRequest("Não autorizado");        
+            }
+            return response.ok(data);
+            
+            
+            
+        }
     }
-
-
